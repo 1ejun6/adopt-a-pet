7c6cee2634f8eb490b2f263a6655d04760d5120a
1ejun6 <lowyinjin@gmail.com>
2026-03-10 16:15:25 +0800

    c: session

--- server.js
+++ server.js
@@ -2,12 +2,25 @@ const express = require('express');
 const server = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const session = require('express-session');
 
 server.use('/', express.static('public'));
 server.use(express.urlencoded({ extended: true }));
 server.set('view engine', 'ejs');
 
 dotenv.config({ path: './.env' });
+
+server.use(session({
+    name: 'sid',
+    secret: process.env.secret,
+    resave: false,
+    saveUninitialized: false
+}));
+
+server.use((req, res, next) => { //on every request
+    res.locals.currentuser = req.session ? req.session.user : null; //temporary storage box for one request
+    next();
+});
 
 async function connectdb() {
     try {
