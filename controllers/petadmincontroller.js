3043807bcf16a52c08849233d5319fb6d35505da
malfianmr <malfian.mr.2025@computing.smu.edu.sg>
2026-03-25 09:20:55 +0800

    u: added pet gender

--- controllers/petadmincontroller.js
+++ controllers/petadmincontroller.js
@@ -18,6 +18,7 @@ exports.createPet = async (req, res) => {
     const name = req.body.name;
     const species = req.body.species;
     const age = req.body.age;
+    const gender = req.body.gender;
     const weight = req.body.weight;
     let health = req.body.health;
     const status = req.body.status;
@@ -38,6 +39,7 @@ exports.createPet = async (req, res) => {
         name,
         species,
         age: Number(age),
+        gender,
         weight: Number(weight),
         health: healthString,
         status,
@@ -108,6 +110,7 @@ exports.updatePet = async (req, res) => {
     const pid = req.params.pid;
     const name = req.body.name;
     const species = req.body.species;
+    const gender = req.body.gender;
     const age = req.body.age;
     const weight = req.body.weight;
     let health = req.body.health;
@@ -125,7 +128,7 @@ exports.updatePet = async (req, res) => {
         healthString = health.join(", ")
     }
 
-    if (!name || !species || !age || !weight || !status) {
+    if (!name || !species || !age || !weight || !status || !gender) {
         const currentPet = await pet.getPetbyPID(pid);
         return res.render("admin/pet/update", { currentPet, m: null, e: "All required fields must be filled in" });
     }
@@ -134,6 +137,7 @@ exports.updatePet = async (req, res) => {
         name,
         species,
         age: Number(age),
+        gender,
         weight: Number(weight),
         health: healthString,
         status,
